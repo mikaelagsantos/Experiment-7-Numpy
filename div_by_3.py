c5762063fabfc190785e475b0ceef0c45a03b9fd
Mikaela G. Santos <57312908+mikaelagsantos@users.noreply.github.com>
2019-11-16 12:59:35 +0800

    Add files via upload

---
+++ div_by_3.py
@@ -0,0 +1,14 @@
+import numpy as nd
+
+#this is the original ndarray
+array = nd.arange(1,101,1).reshape(10,10)
+
+#this is the ndarray squares of the first 100 positive integers
+A = array**2
+print('Array A: \n', A)  
+
+#this determines the elements of array A that are divisible by 3      
+print ('\n Elements in array A that are divisible by 3: \n', A[A%3 == 0])
+
+
+        
